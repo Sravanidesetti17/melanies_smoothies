7643f69446a41505d0e592961ec582530cae35cd
Sravanidesetti17 <163382663+Sravanidesetti17@users.noreply.github.com>
2024-03-15 14:36:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('Fruit
 #st.dataframe(data= my_dataframe ,use_container_width=True)
 #st.stop()
 
-pd_df = dataframe.to_pandas()
+pd_df = my_dataframe.to_pandas()
 st.dataframe(pd_df)
 st.stop()
 
